5b8c1cabda47a4f1dc16ff80837376e7953e1bfd
r8d8 <ckryvomaz@gmail.com>
2018-12-25 16:23:17 +0200

    Update test app

--- src/main.c
+++ src/main.c
@@ -1,28 +1,21 @@
-/*
- * Copyright (c) 2014-2018 Cesanta Software Limited
- * All rights reserved
- *
- * Licensed under the Apache License, Version 2.0 (the ""License"");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an ""AS IS"" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 #include "mgos.h"
 #include "mgos_dht.h"
+#include "mgos_rpc.h"
 
 static void timer_cb(void *dht) {
   LOG(LL_INFO, ("Temperature: %lf", mgos_dht_get_temp(dht)));
 }
 
+static void rpc_cb(struct mg_rpc_request_info *ri, void *cb_arg,
+                   struct mg_rpc_frame_info *fi, struct mg_str args) {
+  mg_rpc_send_responsef(ri, "{value: %lf}", mgos_dht_get_temp(cb_arg));
+  (void) fi;
+  (void) args;
+}
+
 enum mgos_app_init_result mgos_app_init(void) {
   struct mgos_dht *dht = mgos_dht_create(mgos_sys_config_get_app_pin(), DHT22);
   mgos_set_timer(1000, true, timer_cb, dht);
+  mg_rpc_add_handler(mgos_rpc_get_global(), "Temp.Read", "", rpc_cb, dht);
   return MGOS_APP_INIT_SUCCESS;
 }
