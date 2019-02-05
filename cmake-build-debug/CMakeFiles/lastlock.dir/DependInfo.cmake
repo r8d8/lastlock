7288395f7e2e275a89048f3e36894af10216d68b
r8d8 <ckryvomaz@gmail.com>
2019-02-05 23:48:14 +0200

    Update gitignore

---
+++ cmake-build-debug/CMakeFiles/lastlock.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/k2/Workbench/lastlock/mongoose/build/gen/build_info.c" "/home/k2/Workbench/lastlock/cmake-build-debug/CMakeFiles/lastlock.dir/mongoose/build/gen/build_info.c.o"
+  "/home/k2/Workbench/lastlock/mongoose/build/gen/ffi_exports.c" "/home/k2/Workbench/lastlock/cmake-build-debug/CMakeFiles/lastlock.dir/mongoose/build/gen/ffi_exports.c.o"
+  "/home/k2/Workbench/lastlock/mongoose/build/gen/mg_build_info.c" "/home/k2/Workbench/lastlock/cmake-build-debug/CMakeFiles/lastlock.dir/mongoose/build/gen/mg_build_info.c.o"
+  "/home/k2/Workbench/lastlock/mongoose/build/gen/mgos_config.c" "/home/k2/Workbench/lastlock/cmake-build-debug/CMakeFiles/lastlock.dir/mongoose/build/gen/mgos_config.c.o"
+  "/home/k2/Workbench/lastlock/mongoose/build/gen/mgos_deps_init.c" "/home/k2/Workbench/lastlock/cmake-build-debug/CMakeFiles/lastlock.dir/mongoose/build/gen/mgos_deps_init.c.o"
+  "/home/k2/Workbench/lastlock/mongoose/build/gen/mgos_ro_vars.c" "/home/k2/Workbench/lastlock/cmake-build-debug/CMakeFiles/lastlock.dir/mongoose/build/gen/mgos_ro_vars.c.o"
+  "/home/k2/Workbench/lastlock/mongoose/src/main.c" "/home/k2/Workbench/lastlock/cmake-build-debug/CMakeFiles/lastlock.dir/mongoose/src/main.c.o"
+  "/home/k2/Workbench/lastlock/zephyr/boards/arm/qca4020/pinmux.c" "/home/k2/Workbench/lastlock/cmake-build-debug/CMakeFiles/lastlock.dir/zephyr/boards/arm/qca4020/pinmux.c.o"
+  "/home/k2/Workbench/lastlock/zephyr/src/main.c" "/home/k2/Workbench/lastlock/cmake-build-debug/CMakeFiles/lastlock.dir/zephyr/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../mongoose/build/gen"
+  "../zephyr/boards/arm/qca4020"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/lastlock.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lastlock.dir/mongoose/build/gen/build_info.c.o"
+  "CMakeFiles/lastlock.dir/mongoose/build/gen/ffi_exports.c.o"
+  "CMakeFiles/lastlock.dir/mongoose/build/gen/mg_build_info.c.o"
+  "CMakeFiles/lastlock.dir/mongoose/build/gen/mgos_config.c.o"
+  "CMakeFiles/lastlock.dir/mongoose/build/gen/mgos_deps_init.c.o"
+  "CMakeFiles/lastlock.dir/mongoose/build/gen/mgos_ro_vars.c.o"
+  "CMakeFiles/lastlock.dir/mongoose/src/main.c.o"
+  "CMakeFiles/lastlock.dir/zephyr/boards/arm/qca4020/pinmux.c.o"
+  "CMakeFiles/lastlock.dir/zephyr/src/main.c.o"
+  "lastlock.pdb"
+  "lastlock"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lastlock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
